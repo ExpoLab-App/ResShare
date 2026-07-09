594dacf0694437b8bcd98ab3a2b07ec33c7f9372
Devang Borkar <devangborkar3@gmail.com>
2026-07-09 11:30:35 -0700

    fix: wire in port env configuration

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ if __name__ == '__main__':
     import os
 
     debug_mode = os.environ.get('FLASK_ENV', 'production') != 'production'
-    app.run(host='0.0.0.0', port=5000, debug=debug_mode)
+    run_port = int(os.environ.get('FLASK_RUN_PORT', 5000))
+    app.run(host='0.0.0.0', port=run_port, debug=debug_mode)
